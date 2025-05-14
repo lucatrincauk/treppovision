3c1c6604574f3407c32d3dd8020eacf47afd38a2
Luca Trinca <lucatrinca.uk@gmail.com>
2025-05-14 11:25:14 +0000

    not working

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -11,11 +11,11 @@ export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="relative flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 w-full max-w-screen-2xl mx-auto py-8 px-6 sm:px-10 lg:px-16">
+      <main className="flex-1 w-full max-w-screen-xl mx-auto py-8 px-6 sm:px-10 lg:px-16">
         {children}
       </main>
       <footer className="py-6 md:py-0 border-t border-border/40">
-        <div className="w-full max-w-screen-2xl mx-auto px-6 sm:px-10 lg:px-16 flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
+        <div className="w-full max-w-screen-xl mx-auto px-6 sm:px-10 lg:px-16 flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
             Creato per i Fan dell'Eurovision. TreppoVision &copy; {new Date().getFullYear()}.
           </p>
