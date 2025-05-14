a1d1b02cb4a271279b7b30c67f8ba99bf293daa2
Luca Trinca <lucatrinca.uk@gmail.com>
2025-05-14 18:57:00 +0000

    it seems that the header is missing padding left and right. align it to the main content

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,7 @@ import { Music } from "lucide-react";
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="w-full max-w-screen-xl mx-auto px-[30px] flex h-14 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <Music className="h-6 w-6 text-primary" />
           <span className="font-bold sm:inline-block text-lg">
