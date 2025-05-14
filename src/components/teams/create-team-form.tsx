bdebf294293fc61415e0c3463b175b86b90941ed
Luca Trinca <lucatrinca.uk@gmail.com>
2025-05-14 13:36:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/teams/create-team-form.tsx:4:8
    > Parsing ecmascript source code failed
    >   2 | "use client";
    >   3 |
    > > 4 | import ** React from "react";
    >     |        ^^
    >   5 | import { useForm } from "react-hook-form";
    >   6 | import { zodResolver } from "@hookform/resolvers/zod";
    >   7 | import ** z from "zod";
    >
    > Expected 'from', got '**'
    >
    >

--- src/components/teams/create-team-form.tsx
+++ src/components/teams/create-team-form.tsx
@@ -1,10 +1,10 @@
 
 "use client";
 
-import ** React from "react";
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import ** z from "zod";
+import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
