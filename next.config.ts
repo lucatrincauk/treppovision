657c59fa7e2718f2c648453782af6d86c1e788dd
Luca Trinca <lucatrinca.uk@gmail.com>
2025-05-14 11:05:03 +0000

    replace the banner with this one

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com', 
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
